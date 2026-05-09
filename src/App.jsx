cacc35556e0e432d8fcab39960d5e45078bcf002
Kanth <kanthreddybijjam65@gmail.com>
2026-05-09 19:25:06 +0530

    Add notes input section UI

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Navbar from "./Components/Navbar";
+import NotesInput from "./Components/NotesInput";
 
 function App() {
   return (
     <>
       <Navbar />
+      <NotesInput />
     </>
   );
 }
---
+++ src/Components/NotesInput.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function NotesInput (){
+  return (
+    <div className='notes-input-container'>
+        <input type="text" placeholder='Enter a Note' />     
+        <button> Add </button> 
+    </div>
+  )
+}
+
+export default NotesInput
