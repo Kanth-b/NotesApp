f7fee311894dd4aa0eaa9b4e4e10c13d7049bd25
Kanth <kanthreddybijjam65@gmail.com>
2026-05-10 11:10:07 +0530

    Implement note input state handling

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Navbar from "./Components/Navbar";
 import NotesInput from "./Components/NotesInput";
+import { useState } from "react";
 
 function App() {
+  const [notes, setNotes] = useState(null);
   return (
     <>
       <Navbar />
-      <NotesInput />
+      <NotesInput notes={notes} setNotes={setNotes} />
     </>
   );
 }
--- src/Components/NotesInput.jsx
+++ src/Components/NotesInput.jsx
@@ -1,12 +1,33 @@
-import React from 'react'
+import React from "react";
+import { useState } from "react";
 
-function NotesInput (){
+function NotesInput({ notes, setNotes }) {
+  const [input, setInput] = useState(null);
+
+  function handleAddNotes() {
+    if (input.trim() === "") {
+      return;
+    }
+    setNotes({
+      ...notes,
+      input,
+    });
+
+    setInput("");
+  }
   return (
-    <div className='notes-input-container'>
-        <input type="text" placeholder='Enter a Note' />     
-        <button> Add </button> 
+    <div className="notes-input-container">
+      <input
+        type="text"
+        placeholder="Enter a Note"
+        value={input}
+        onChange={(e) => {
+          setInput(e.target.value);
+        }}
+      />
+      <button onClick={handleAddNotes}> Add </button>
     </div>
-  )
+  );
 }
 
-export default NotesInput
+export default NotesInput;
